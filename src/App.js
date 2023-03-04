d046cd598d77e36de20e9837dc3b82f32ae0e9bb
shortBread2329 <59463678+shortBread2329@users.noreply.github.com>
2023-03-04 08:38:11 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          うえーいGitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+import React from "react";
+import { useState } from "react";
+
+const App = (props) => {
+  const [memo, setMemo] = useState("tomato");
+
+  const changeMemo = (e) => setMemo(e.target.value);
+
+  return(
+    <div>
+      <div>{memo}</div>
+      <textarea onChange={changeMemo} val={memo}></textarea>
     </div>
-  );
+  )
 }
 
 export default App;
